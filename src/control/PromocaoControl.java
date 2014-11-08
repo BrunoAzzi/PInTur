90f1475b05d0e02c292e013cb98cb6cbd9ec04fe
everton_m <everton_m@SNFNS2EP04VM042.florianopolis.sc.senai.br>
2014-11-07 22:00:29 -0200

    Implementacao da classe de controle PromocaoControl, e tambem implementacao da view CadastroPromocao.

---
+++ src/control/PromocaoControl.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import java.util.ArrayList;
+import javax.persistence.NoResultException;
+import javax.swing.JOptionPane;
+import messages.Warnings;
+import model.Promocao;
+
+/**
+ *
+ * @author everton_m
+ */
+public class PromocaoControl {
+    
+    public static ArrayList<Promocao> selectAll(){
+        return new ArrayList(Conexao.namedQuery("Promocao.findAll"));
+    }
+
+    public static Promocao findByCodigo(Integer codigo) throws NullPointerException {
+        if (codigo == null) {
+            throw new NullPointerException("O Código está vazio");
+        }
+        try {
+            return (Promocao) Conexao.singleResultNamedQuery("Promocao.findByCodigo", codigo, "codigo");
+        } catch (NoResultException e) {
+            JOptionPane.showMessageDialog(null, Warnings.OBJETO_NAO_ENCONTRADO.getMensagem(), JOptionPane.MESSAGE_PROPERTY, JOptionPane.WARNING_MESSAGE);
+            return null;
+        }
+    }
+        
+    public static void add(Promocao produto){
+       Conexao.persist(produto);
+    }
+    
+    public static void deleteByCodigo(Integer codigo){
+        if(codigo == null){
+            throw new NullPointerException("O Código está vazio");
+        }
+        Conexao.remove(codigo);
+    }     
+    
+}
