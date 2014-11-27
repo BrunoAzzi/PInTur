98be2efb69035a9e11398e79e3177ed4a339954a
bruno_azzi <bruno_azzi@SNFNS2EP04VM177.florianopolis.sc.senai.br>
2014-11-26 22:25:16 -0200

    Implementada  a parte de remoção e minnor fixes em usuario.

--- src/view/usuarios/ListarUsuarios.java
+++ src/view/usuarios/ListarUsuarios.java
@@ -39,7 +39,7 @@ public class ListarUsuarios extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setResizable(false);
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowActivated(java.awt.event.WindowEvent evt) {
